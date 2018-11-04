47fb4d742de4b0ab59c1d482d5d07d80564733d5
Johnny Kingi / 10005351 <jknz84@gmail.com>
2018-11-04 18:58:31 +1300

    Products database created with Create/Read/Edit functionality pages/index.html.erb now reading inforamtion for products from database

--- bootstrap_play/app/controllers/pages_controller.rb
+++ bootstrap_play/app/controllers/pages_controller.rb
@@ -2,4 +2,14 @@ class PagesController < ApplicationController
     def index
 
     end
+    def about
+        @title = 'About Us';
+    end
+    def downloads
+
+    end
+
+    def product
+
+    end    
 end
